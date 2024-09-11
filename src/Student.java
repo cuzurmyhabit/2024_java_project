2fcc1599f6f032da84f60f10bb297d0a5dcdde5c
지수민 <s2472@e-mirim.hs.kr>
2024-09-11 16:37:40 +0900

    로

--- src/Student.java
+++ src/Student.java
@@ -2,7 +2,7 @@ public class Student extends User {
     public Student(String room, String grade, String classRoom, String name) {
         super(room, grade, classRoom, name);
     }
-
+    
     @Override
     public boolean authenticate(String room, String grade, String classRoom, String name) {
         return this.room.equals(room) &&
